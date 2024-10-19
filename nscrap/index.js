9ce0efff2b8b847a8fb5fafcbdcba4ea9910cfab
Khalid Azmatullah <khalid.studymaterial@gmail.com>
2024-10-19 18:54:56 +0530

    prototype-nscrap

--- nscrap/index.js
+++ nscrap/index.js
@@ -6,24 +6,30 @@ const browser = await puppeteer.launch();
 const page = await browser.newPage();
 
 // Navigate the page to a URL.
-await page.goto('https://developer.chrome.com/');
+await page.goto('https://timesofindia.indiatimes.com/');
 
 // Set screen size.
 await page.setViewport({width: 1080, height: 1024});
 
 // Type into search box.
-await page.locator('.devsite-search-field').fill('automate beyond recorder');
+await page.locator('.OG1TB').click();
 
+const searchQuery = '.textbox';
+await page.waitForSelector(searchQuery);
+await page.locator('.textbox').fill('tcs');
+
+await page.locator('.submit').click();
 // Wait and click on first result.
-await page.locator('.devsite-result-item-link').click();
 
+
+await page
 // Locate the full title with a unique string.
 const textSelector = await page
-  .locator('text/Customize and automate')
+  .locator('text/Tata Consultancy Services')
   .waitHandle();
 const fullTitle = await textSelector?.evaluate(el => el.textContent);
 
 // Print the full title.
-console.log('The title of this blog post is "%s".', fullTitle);
+console.log('Text: "%s".', fullTitle);
 
 await browser.close();
